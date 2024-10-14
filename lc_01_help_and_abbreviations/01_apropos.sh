9da3059b5d39d3ade1c9c6ee0bd8889dbffa70f0
Kushagra Raj Tiwari <kushagra.tiwari@marketcast.com>
2024-10-14 16:12:25 +0530

    Adding example for apropos.
    Signed-off-by: Kushagra Raj Tiwari <kushagra.tiwari@marketcast.com>

---
+++ lc_01_help_and_abbreviations/01_apropos.sh
@@ -0,0 +1,6 @@
+# In Linux, the apropos command is used to search the manual page descriptions for a keyword. It helps you find commands related to a specific topic.
+# For example ,If you want to find commands related to "network," you would use:
+
+apropos network > 01_apropos_output.txt
+
+# This will list all the commands and descriptions that include the word "network."
